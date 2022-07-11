7abc722a10d66ed77b82c02395f1e6bb75e35cec
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-07-10 22:13:27 -0300

    feat: desafio

--- src/desafioBot.js
+++ src/desafioBot.js
@@ -7,16 +7,12 @@ bot.start((ctx) => {
   const { from } = ctx.update.message;
 
   console.log(from);
-  ctx.reply(`Seja bem vindo, ${from.first_name}`);
-});
 
-bot.on("text", async (ctx, next) => {
-  await ctx.reply("Mid 1");
-  next();
-});
-
-bot.on("text", async (ctx) => {
-  await ctx.reply("Mid 2");
+  if (from.id == process.env.MY_ID) {
+    ctx.reply(`Seja bem vindo, mestre ${from.first_name}!`);
+  } else {
+    ctx.reply("Usuário desconhecido, adeus!");
+  }
 });
 
 bot.startPolling();
